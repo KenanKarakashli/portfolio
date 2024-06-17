77df8162f787f42746ca3c26e9c914855e7a4086
Kenan <51777830+KenanKarakashli@users.noreply.github.com>
2024-06-18 00:13:20 +0400

    Commit message

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let secCount = document.querySelector(".sec_count");
 
 let count = 0;
-setInterval(function(){
+let myInterval = setInterval(function(){
   let d = new Date();
   let secondsHolder = d.getSeconds();
-  let paddedCount = count.toString().padStart(3,'0');
-  secCount.textContent = `${paddedCount} seconds.`;
+    let paddedCount = count.toString().padStart(3,'0');
+    secCount.textContent = `${paddedCount} seconds.`;
   count++
+    if(count === 999){
+      count = 0;
+    }
 },1000);
+
